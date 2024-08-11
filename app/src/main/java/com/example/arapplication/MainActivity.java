fe2282a3513f7da0d47ae026744b49e2d4be8127
Ramanand <ramanand.in7@gmail.com>
2024-08-11 12:13:14 +0530

    tab layout

--- app/src/main/java/com/example/arapplication/MainActivity.java
+++ app/src/main/java/com/example/arapplication/MainActivity.java
@@ -7,14 +7,33 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout mainTab ;
+    private ViewPager mainVP ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        mainTab = findViewById(R.id.mainTab);
+        mainVP = findViewById(R.id.mainViewpager);
+
+        mainTab.setupWithViewPager(mainVP);
+
+        VPadapter vpAdapter = new VPadapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        vpAdapter.addFragment(new currentYearFragment(),"CurrentYear");
+        vpAdapter.addFragment(new lastYearFragment(),"LastYear");
+        mainVP.setAdapter(vpAdapter);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
